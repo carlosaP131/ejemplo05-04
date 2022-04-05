37aa147c92a4342a0552ad7fbb4ee4eca41d0cfd
carlos <carlos.cantrax303@gmail.com>
2022-04-05 16:38:27 -0500

    commmit 2

--- src/objeto/Triangulo.java
+++ src/objeto/Triangulo.java
@@ -2,19 +2,19 @@
  *Autor:carlos Aurelio Alcántara Pérez 
  *Fecha de creacion: 05-04-2022 ***
  *Fecha de actualizacion:05-04-2022
- *Descripicion:este programa para calcular el area de un rectangulo 
+ *Descripicion:este programa da dos opciones para calcular el area de  un triangulo 
  **                                                                                                 
  * *************************************************************************************************/
 package objeto;
 
-public class Rectangulo {
-     private double base;
-     private double altura;
+public class Triangulo {
+	private double base;
+    private double altura;
 
-     public Rectangulo(){
-    	 
-     }
-	public Rectangulo(double base, double altura) {
+    public Triangulo(){
+   	 
+    }
+	public Triangulo(double base, double altura) {
 		
 		this.base = base;
 		this.altura = altura;
